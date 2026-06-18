b397f5c1a0a3c9e031f1654fe6ad2eb8e22d3fd9
angel <angel_guerrero1@ucne.edu.do>
2026-06-17 20:03:49 -0400

    Refactoriza AmonestacionFormViewModel y agrega @Inject a los casos de uso

--- app/src/main/java/com/android/angelraonel_guerreroantigua_ap2_p1/domain/amonestacion/usecase/GetAmonestacionUseCase.kt
+++ app/src/main/java/com/android/angelraonel_guerreroantigua_ap2_p1/domain/amonestacion/usecase/GetAmonestacionUseCase.kt
@@ -1,8 +1,9 @@
 package com.android.angelraonel_guerreroantigua_ap2_p1.domain.amonestacion.usecase
 
 import com.android.angelraonel_guerreroantigua_ap2_p1.domain.amonestacion.repository.AmonestacionRepository
+import javax.inject.Inject
 
-class GetAmonestacionUseCase(
+class GetAmonestacionUseCase @Inject constructor(
 private val repository: AmonestacionRepository
 ){
     suspend operator fun invoke(id: Int) = repository.getAmonestacion(id)
--- app/src/main/java/com/android/angelraonel_guerreroantigua_ap2_p1/domain/amonestacion/usecase/ObserveAmonestacionUseCase.kt
+++ app/src/main/java/com/android/angelraonel_guerreroantigua_ap2_p1/domain/amonestacion/usecase/ObserveAmonestacionUseCase.kt
@@ -3,8 +3,9 @@ package com.android.angelraonel_guerreroantigua_ap2_p1.domain.amonestacion.useca
 import com.android.angelraonel_guerreroantigua_ap2_p1.domain.amonestacion.model.Amonestacion
 import com.android.angelraonel_guerreroantigua_ap2_p1.domain.amonestacion.repository.AmonestacionRepository
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
-class ObserveAmonestacionUseCase(
+class ObserveAmonestacionUseCase @Inject constructor(
     private var repository: AmonestacionRepository
 ){
     operator fun invoke(): Flow<List<Amonestacion>> = repository.observeAmonestacion()
