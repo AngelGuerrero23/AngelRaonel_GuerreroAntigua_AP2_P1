f1565a9b3bc54b091e632a54aa83a5ddfb6e9f0f
angel <angel_guerrero1@ucne.edu.do>
2026-06-17 20:09:38 -0400

    Cambia propiedades de Amonestacion a val

--- app/src/main/java/com/android/angelraonel_guerreroantigua_ap2_p1/domain/amonestacion/model/Amonestacion.kt
+++ app/src/main/java/com/android/angelraonel_guerreroantigua_ap2_p1/domain/amonestacion/model/Amonestacion.kt
@@ -2,7 +2,7 @@ package com.android.angelraonel_guerreroantigua_ap2_p1.domain.amonestacion.model
 
 data class Amonestacion(
     val amonestacionId: Int =0,
-    var nombre: String? =" ",
-    var razon: String? =" ",
-    var monto: Double = 0.0
+    val nombre: String? =" ",
+    val razon: String? =" ",
+    val monto: Double = 0.0
 )
